3007533befe935dc0ef7c0d0c070391ea9cf5a47
siampathan005@gmail.com <siampathan005@gmail.com>
2023-10-21 23:00:45 +0600

    add search filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import {
   SpaceXDataProvider,
@@ -7,6 +8,10 @@ import dayjs from "dayjs";
 
 const App: React.FC = () => {
   const { data, loading, error } = useSpaceXData();
+  const [search, setSearch] = useState("");
+  console.log(search);
+
+  console.log(data);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -18,26 +23,35 @@ const App: React.FC = () => {
 
   return (
     <>
-      <h1>Fetched Data: Type Script and UseContext</h1>
+      <h1>Fetched Data:</h1>
+      <div className="search-section">
+        <input type="text" onChange={(e) => setSearch(e.target.value)} />
+      </div>
 
       <div className="my-div">
-        {data.map((item, index) => (
-          <div className="cover-sec" key={index}>
-            <div className="image_container">
-              <img src={item.links.mission_patch_small} alt="image" />
+        {data
+          .filter((item) => {
+            return search.toLocaleLowerCase() === ""
+              ? item
+              : item.rocket.rocket_name.toLocaleLowerCase().includes(search);
+          })
+          .map((item, index) => (
+            <div className="cover-sec" key={index}>
+              <div className="image_container">
+                <img src={item.links.mission_patch_small} alt="image" />
+              </div>
+              <p>
+                Launch Date:
+                {dayjs(item.launch_date_local).format("D MMMM, YYYY")}
+              </p>
+              <h3>{item.mission_name}</h3>
+              <p> {item.rocket.rocket_name} </p>
+              <p>Launch Status:</p>
+              <p className={`${item.launch_success ? "green" : "red"}`}>
+                {item.launch_success ? "Success" : "Failed"}
+              </p>
             </div>
-            <p>
-              Launch Date:
-              {dayjs(item.launch_date_local).format("D MMMM, YYYY")}
-            </p>
-            <h3>{item.mission_name}</h3>
-            <p> {item.rocket.rocket_name} </p>
-            <p>Launch Status:</p>
-            <p className={`${item.launch_success ? "green" : "red"}`}>
-              {item.launch_success ? "Success" : "Failed"}
-            </p>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
